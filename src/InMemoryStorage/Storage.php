e62715970dc77e7641f8d324cca68d52efbc1ad4
mjsilva <mail@manueljoaosilva.com>
2013-09-02 00:49:03 +1000

    first commit

---
+++ src/InMemoryStorage/Storage.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace InMemoryStorage;
+
+
+/**
+ * Class Storage
+ *
+ * @package InMemoryStorage
+ */
+class Storage
+{
+    /**
+     * @var Storage
+     */
+    protected static $instance = null;
+    /**
+     * @var array
+     */
+    protected $storage = array();
+
+    protected function __construct()
+    {
+        // Thou shalt not construct that which is unconstructable!
+    }
+
+    protected function __clone()
+    {
+        // Me not like clones! Me smash clones!
+    }
+
+    public static function getInstance()
+    {
+        if (!isset(static::$instance)) {
+            static::$instance = new static;
+        }
+
+        return static::$instance;
+    }
+
+    /**
+     * @param string $name
+     *
+     * @return mixed
+     * @throws \Exception
+     */
+    public function get($name)
+    {
+        if (!isset($this->storage[$name])) {
+            throw new \Exception("Value $name not found in storage");
+        }
+
+        return $this->storage[$name];
+    }
+
+    /**
+     * @param string $name
+     * @param mixed  $value
+     */
+    public function set($name, $value)
+    {
+        $this->storage[$name] = $value;
+    }
+}
